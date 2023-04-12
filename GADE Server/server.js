5df9c022429cf5f39056dee991f6bcf172f25b55
Ekyoz <alexandre.tresallet@gmail.com>
2023-04-12 23:14:37 +0200

    create auth systeme

--- GADE Server/server.js
+++ GADE Server/server.js
@@ -1,23 +1,62 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-const http = require('http')
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const app = express();
 
-const serverApp = express()
+const data = require("./data.json");
+const users = data.server.users;
+const host = data.server.host;
+const port = data.server.port;
 
-let port = 8080
-let host = "192.168.1.28"
+// Middleware pour parser les données de la requête
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-serverApp.use(bodyParser.urlencoded({ extended: false }));
-serverApp.use(bodyParser.json())
+// Middleware pour gérer la session
+app.use(session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false
+}));
 
-serverApp.get('/', (req, res) => {
-    res.status(200).send('ON');
-    console.log("ON")
-})
+// Middleware pour l'authentification
+function requireAuth(req, res, next) {
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        return res.status(401).send("Unauthorized");
+    }
+}
 
-serverApp.set('port', 8080);
-const server = http.createServer(serverApp);
+// Route pour l'authentification
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.user === username && u.password === password);
+    if (user) {
+        req.session.user = user;
+        res.send(`Logged in successfully with user: ${req.session.user.user}`);
+    } else {
+        res.status(401).send("Invalid username or password");
+    }
+});
 
-server.listen(port, host, () => {
+// Route pour récupérer les informations de l'utilisateur courant
+app.get("/user", requireAuth, (req, res) => {
+    res.send(req.session.user);
+});
+
+// Route pour se déconnecter
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.send("Logged out successfully");
+});
+
+// Route de test
+app.get("/", requireAuth, (req, res) => {
+    res.send(`Hello! You're connected with: ${req.session.user.user}`);
+});
+
+// Démarrage du serveur
+app.listen(3000, () => {
     console.log(`Server is running on http://${host}:${port}`)
-})
+});
--- GADE Server/test.js
+++ GADE Server/test.js
@@ -1,30 +1,62 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-const https = require('https');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const app = express();
 
-const options = {
-    key: fs.readFileSync('auth/key.pem'),
-    cert: fs.readFileSync('auth/cert.pem')
-};
+const data = require("./data.json");
+const users = data.server.users;
+const host = data.server.host;
+const port = data.server.port;
 
-const serverApp = express()
+// Middleware pour parser les données de la requête
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-let port = 8080
-let host = "192.168.1.28"
+// Middleware pour gérer la session
+app.use(session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false
+}));
 
-serverApp.use(bodyParser.urlencoded({ extended: false }));
-serverApp.use(bodyParser.json())
+// Middleware pour l'authentification
+function requireAuth(req, res, next) {
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        return res.status(401).send("Unauthorized");
+    }
+}
 
-serverApp.get('/', (req, res) => {
-    res.status(200).send('ON');
-    console.log("ON")
-})
+// Route pour l'authentification
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.user === username && u.password === password);
+    if (user) {
+        req.session.user = user;
+        res.send(`Logged in successfully with user: ${req.session.user.user}`);
+    } else {
+        res.status(401).send("Invalid username or password");
+    }
+});
 
-const server = https.createServer(options, serverApp);
+// Route pour récupérer les informations de l'utilisateur courant
+app.get("/user", requireAuth, (req, res) => {
+    res.send(req.session.user);
+});
 
-const PORT = process.env.PORT || 3000;
+// Route pour se déconnecter
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.send("Logged out successfully");
+});
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Route de test
+app.get("/", requireAuth, (req, res) => {
+    res.send(`Hello! You're connected with: ${req.session.user.user}`);
+});
+
+// Démarrage du serveur
+app.listen(3000, () => {
+    console.log(`Server is running on http://${host}:${port}`)
 });
